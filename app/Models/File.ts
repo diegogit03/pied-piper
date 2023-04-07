a4ee013b2c2943a8b4ad1a03d694ce27fc1f0a2a
Diego <dhom91718@gmail.com>
2023-04-07 03:21:16 -0300

    creating models

---
+++ app/Models/File.ts
@@ -0,0 +1,26 @@
+import { DateTime } from 'luxon'
+import { BaseModel, BelongsTo, belongsTo, column } from '@ioc:Adonis/Lucid/Orm'
+import Folder from './Folder'
+
+export default class File extends BaseModel {
+  @column({ isPrimary: true })
+  public id: number
+
+  @column()
+  public name: string
+
+  @column()
+  public type: string
+
+  @column()
+  public folderId: number
+
+  @column.dateTime({ autoCreate: true })
+  public createdAt: DateTime
+
+  @column.dateTime({ autoCreate: true, autoUpdate: true })
+  public updatedAt: DateTime
+
+  @belongsTo(() => Folder)
+  public folder: BelongsTo<typeof Folder>
+}
