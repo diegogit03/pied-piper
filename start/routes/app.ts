f4147372079bd45f1772fd3dc2f8903d0ac2f1b2
Diego <dhom91718@gmail.com>
2023-04-15 04:01:01 -0300

    creating routes

--- start/routes/app.ts
+++ start/routes/app.ts
@@ -1,9 +1,16 @@
 import Route from '@ioc:Adonis/Core/Route'
 
 Route.group(() => {
-  Route.get('/files', 'FilesController.index').as('files.index')
-  Route.post('/files', 'FilesController.store').as('files.store')
-  Route.delete('/files/:id', 'FilesController.destroy').as('files.destroy')
+  Route.group(() => {
+    Route.post('/:folder', 'FoldersController.store').as('store')
+    Route.delete('/:folder', 'FoldersController.destroy').as('destroy')
+
+    Route.get('/:folder/files', 'FilesController.index').as('files.index')
+    Route.post('/:folder/files', 'FilesController.store').as('files.store')
+    Route.delete('/:folder/files/:id', 'FilesController.destroy').as('files.destroy')
+  })
+    .as('folders')
+    .prefix('folders')
 
   Route.get('/shared', 'SharedController.index').as('shared.index')
 })
